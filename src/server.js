da427a37e2aa65f3c99a13a3136de109472964b5
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-24 07:58:28 -0300

    configurado o servidor com acesso ao Public HTML

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ const express = require("express")
 const server = express()
 
 // paths
+server.use(express.static("public"))
 
 // HOME
 server.get("/", (req, res) => {
     res.sendfile(__dirname + "/views/index.html")
 })
-
 
 // Server turn on
 server.listen(3000)
