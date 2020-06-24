14579abd16364f8a73a790255e0d0a4708fa7378
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-24 08:04:47 -0300

    rotas configuradasdo JS

--- src/server.js
+++ src/server.js
@@ -9,5 +9,13 @@ server.get("/", (req, res) => {
     res.sendfile(__dirname + "/views/index.html")
 })
 
+server.get("/account", (req, res) => {
+    res.sendfile(__dirname + "/views/account.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendfile(__dirname + "/views/search-results.html")
+})
+
 // Server turn on
 server.listen(3000)
