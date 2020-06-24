f079d2fb5247e9d0373aff2a443806fce23b1f68
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-24 06:13:22 -0300

    nodemon dep instalado

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const server = express()
 
+// paths
+
+// HOME
+server.get("/", (req, res) => {
+    req.send("Hi ppl")
+})
+
+
 // Server turn on
 server.listen(3000)
